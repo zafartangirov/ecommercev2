b36a964448ffc6f51f44bcf5dac7ce677e241c00
Zafar <a.b@gmail.com>
2025-03-28 19:17:49 +0500

    updated many files

--- src/main/java/com/example/demo/config/DataLoader.java
+++ src/main/java/com/example/demo/config/DataLoader.java
@@ -2,9 +2,11 @@ package com.example.demo.config;
 
 
 import com.example.demo.entity.Role;
+import com.example.demo.entity.RoleName;
 import com.example.demo.entity.User;
 import com.example.demo.repo.RoleRepository;
 import com.example.demo.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -12,7 +14,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Component
+@Component
 public class DataLoader implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
@@ -25,22 +27,43 @@ public class DataLoader implements CommandLineRunner {
         this.userRepository = userRepository;
     }
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddl;
+
     @Override
     public void run(String... args) throws Exception {
-        List<Role> allRoles = roleRepository.findAll();
-        User user1 = User.builder()
-                .roles(new ArrayList<>(List.of(allRoles.get(0))))
-                .username("a")
-                .password(passwordEncoder.encode("root123"))
-                .fullName("Eshmat Toshmatov")
-                .build();
-        userRepository.save(user1);
-        User user2 = User.builder()
-                .roles(new ArrayList<>(List.of(allRoles.get(1))))
-                .username("b")
-                .password(passwordEncoder.encode("root123"))
-                .fullName("Hikmat Nusratov")
-                .build();
-        userRepository.save(user2);
+        if (ddl.equals("create")) {
+            Role role1 = Role.builder()
+                    .roleName(RoleName.ROLE_ADMIN).build();
+            roleRepository.save(role1);
+            Role role2 = Role.builder()
+                    .roleName(RoleName.ROLE_USER).build();
+            roleRepository.save(role2);
+            Role role3 = Role.builder()
+                    .roleName(RoleName.ROLE_SUPER_ADMIN).build();
+            roleRepository.save(role3);
+            List<Role> allRoles = roleRepository.findAll();
+            User user1 = User.builder()
+                    .roles(new ArrayList<>(List.of(allRoles.get(0))))
+                    .username("a")
+                    .password(passwordEncoder.encode("root123"))
+                    .fullName("Eshmat Toshmatov")
+                    .build();
+            userRepository.save(user1);
+            User user2 = User.builder()
+                    .roles(new ArrayList<>(List.of(allRoles.get(1))))
+                    .username("b")
+                    .password(passwordEncoder.encode("root123"))
+                    .fullName("Hikmat Nusratov")
+                    .build();
+            userRepository.save(user2);
+            User user3 = User.builder()
+                    .roles(new ArrayList<>(List.of(allRoles.get(2))))
+                    .username("c")
+                    .password(passwordEncoder.encode("root123"))
+                    .fullName("Zafar Tangirov")
+                    .build();
+            userRepository.save(user3);
+        }
     }
 }
--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,6 +12,7 @@ import org.springframework.security.core.GrantedAuthority;
 @NoArgsConstructor
 @Entity
 @Table(name = "roles")
+@Builder
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
