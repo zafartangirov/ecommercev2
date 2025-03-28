709879cf5f1b59bf2f973e3c1c4cc56e8068931d
Zafar Tangirov <104876114+zafartangirov@users.noreply.github.com>
2025-03-28 19:36:10 +0500

    Update DataLoader.java

--- src/main/java/com/example/demo/config/DataLoader.java
+++ src/main/java/com/example/demo/config/DataLoader.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+// @Component
 public class DataLoader implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
