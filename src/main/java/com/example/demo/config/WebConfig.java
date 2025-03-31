17e5f89f96382d1139f836bcd64952d657dcf9a7
Zafar Tangirov <104876114+zafartangirov@users.noreply.github.com>
2025-03-31 14:25:40 +0500

    Update WebConfig.java

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://ecommercev1.up.railway.app")
+                .allowedOrigins("https://ecommercev1.up.railway.app/**")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
